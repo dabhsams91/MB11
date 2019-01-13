81bd0bc84adffeded2fc3a3454654c618936763c
yoges <yoges@DESKTOP-V61QEJH>
2019-01-13 08:42:44 -0800

    integration

---
+++ src/main/java/com/mb11/application/service/sport/SportAPIService.java
@@ -0,0 +1,30 @@
+package com.mb11.application.service.sport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.mb11.application.config.RestTemplateConfig;
+
+import net.minidev.json.JSONObject;
+
+@Service
+public class SportAPIService {
+
+	@Autowired
+    RestTemplate restTemplate;
+	
+	private String competitionsUrl = "competitions";
+
+	public void getSeries() {
+		
+		StringBuilder baseUrl = RestTemplateConfig.getBaseURL();
+		baseUrl.append("/"+competitionsUrl);
+		baseUrl.append("?token="+RestTemplateConfig.apiTocken);
+		
+		JSONObject jsonObject = restTemplate.getForObject(baseUrl.toString(), JSONObject.class);
+		
+		System.out.println(jsonObject.toJSONString());
+		
+	}
+}
